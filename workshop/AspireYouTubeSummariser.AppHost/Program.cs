0c270a785764a3b7a8edd0cd0ba109954ed87c49
tlsalsco41122 <tlsalsco41122@gmail.com>
2024-08-06 15:26:36 +0900

    Add GitHub Actions workflow for Azure deployment

--- workshop/AspireYouTubeSummariser.AppHost/Program.cs
+++ workshop/AspireYouTubeSummariser.AppHost/Program.cs
@@ -5,13 +5,13 @@ var cache = builder.AddRedis("cache");
 var config = builder.Configuration;
 
 var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("apiapp")
-                    .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
-                    .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
-                    .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
+                     .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
+                     .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
+                     .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
 
 builder.AddProject<Projects.AspireYouTubeSummariser_WebApp>("webapp")
        .WithExternalHttpEndpoints()
        .WithReference(cache)
        .WithReference(apiapp);
-// 테스트하려고 넣은 주석
+
 builder.Build().Run();
--- workshop/AspireYouTubeSummariser.WebApp/Program.cs
+++ workshop/AspireYouTubeSummariser.WebApp/Program.cs
@@ -12,7 +12,10 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 // builder.Services.AddHttpClient<IApiAppClient, ApiAppClient>(p => p.BaseAddress = new Uri("http://localhost:5050"));
-builder.Services.AddHttpClient<IApiAppClient, ApiAppClient>(p => p.BaseAddress = new Uri("https+http://apiapp"));
+builder.Services.AddHttpClient<IApiAppClient, ApiAppClient>(p => {
+    p.BaseAddress = new Uri("https+http://apiapp");
+    p.Timeout = TimeSpan.FromSeconds(200);
+});
 
 var app = builder.Build();
 
